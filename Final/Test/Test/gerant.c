fa7a636decd56674558c051d7bd0b8f2659d9bea
Glen Roger <glennytokyroger@gmail.com>
2022-12-06 20:14:46 +0100

    update

--- Final/Test/Test/gerant.c
+++ Final/Test/Test/gerant.c
@@ -50,30 +50,62 @@ void add(produit* new){
     scanf("%i",&new->stocks);
 }
 
-void enleve(produit* new, produit* p_new, int n){
-    char* sup;
-    printf("Quel produit voulez-vous enlever ?\n");
-    scanf("%s",sup);
-    for(int i=0;i<n;i++){
-        if(ordrealpha(new[i].nom,sup)!=0){
-            p_new[i] = new[i];
-        }
+/*int add_bin(char* nomfichier){
+    FILE* stock = fopen("stock","ab");
+    produit* new = malloc(sizeof(produit));
+    add(new);
+    fwrite(new,sizeof(produit),1,stock);
+    fclose(stock);
+    free(new);
+    char rep;
+    printf("Voulez vous ajouter d'autres produits ?\n");
+    printf("Choisir (O)ui ou (N)on\n");
+    scanf("%s",&rep);
+    if(rep=='N'){
+        return 0;
     }
+}*/
+
+void enleve(produit* new, int n){
+    char sup;
+    printf("Quel produit voulez-vous enlever ?\n");
+    scanf("%s",&sup);
+    int indice=0;
+    while(ordrealpha(new[indice].nom,&sup)!=0 && indice<n){
+        indice++;
+    }
+    printf("%i\n",indice);
+    /*for(int i=0;i<n-1;i++){
+        if(i<indice){
+            for(int j=0;j<strlen(new[i].nom);j++){
+                new[i].nom[j] = new[i].nom[j];
+            }
+            new[i].prix_unit = new[i].prix_unit;
+            new[i].stocks = new[i].stocks;
+        }
+        else{
+            for(int j=0;j<strlen(new[i].nom);j++){
+                new[i].nom[j] = new[i+1].nom[j];
+            }
+            new[i].prix_unit = new[i+1].prix_unit;
+            new[i].stocks = new[i+1].stocks;
+        }
+    }*/
 }
 
 int main(){
     FILE* stock = fopen("stock","ab");
-    //produit* new = malloc(sizeof(produit));
-    //add(new);
-    //fwrite(new,sizeof(produit),1,stock);
+    produit* new = malloc(sizeof(produit));
+    add(new);
+    fwrite(new,sizeof(produit),1,stock);
     fclose(stock);
-    //free(new);
+    free(new);
     int n = taille("stock");
+    //add_bin("stock");
     produit* tab = malloc(n*sizeof(produit));
     charge(tab,n,"stock");
-    produit* p_new = malloc((n-1)*sizeof(produit));
-    enleve(tab,p_new,n);
-    print_tab(p_new, n);
+    print_tab(tab,n);
+    enleve(tab,n);
+    print_tab(tab,n-1);
     free(tab);
-    free(p_new);
 }
